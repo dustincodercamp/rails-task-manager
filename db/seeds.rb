5b52413eff1d5571c568a366d030c5ced409d425
Dustin Cotcamp <dustincodercamp@gmail.com>
2017-08-08 16:02:13 -0300

    Task Manager complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Task.create(title: "Wash dishes")
+Task.create(title: "Cook food")
+Task.create(title: "Take out the trash")
+Task.create(title: "Sweep the floor")
+Task.create(title: "Get groceries")
